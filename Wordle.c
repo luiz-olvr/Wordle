b431d5130f1160ecceaf0ab9c592e944e9674e12
Luiz Otávio da Rosa de Oliveira <luizotaviodarosadeoliveira@gmail.com>
2024-03-18 22:24:02 -0300

    Atualização v1
    Pergunta se quer jogar novamente e incrementação das letras erradas(teste)

--- Wordle.c
+++ Wordle.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-//Não esqueça do arquivo infaplic.h
-//#include "infaplic.h"
+
 
 #define TAMANHO_PALAVRA 5
 #define MAX_TENTATIVAS 6
@@ -10,10 +9,13 @@
 char palavraChave [5]= "audio";
 char tentativa[TAMANHO_PALAVRA];
 int tentativas = 0;
+int err = 0;
+int tent = 1;
 
 int main(void)
 {
     printf("Bem-vindo ao Wordle (Termo)!\n");
+    while(tent == 1){
     
     while(tentativas < MAX_TENTATIVAS) {
         printf("\n\nTentativa %d de %d\n", tentativas + 1, MAX_TENTATIVAS);
@@ -34,12 +36,17 @@ int main(void)
         // Implementar a lógica de verificação da tentativa aqui
         
          for (int i = 0; i < TAMANHO_PALAVRA; i++) {
-             for(int j = 0; j < TAMANHO_PALAVRA; j++){
-            if (tentativa[i] == palavraChave[j] && i == j) {
-                printf("*");}
+            err = 0;
+            for(int j = 0; j < TAMANHO_PALAVRA; j++){
+                if (tentativa[i] == palavraChave[j] && i == j) {
+                    printf("*");}
             
-            if(tentativa[i] == palavraChave[j] && i != j){
-                printf("+");}
+                if(tentativa[i] == palavraChave[j] && i != j){
+                    printf("+");}
+
+                if(tentativa[i] != palavraChave[j] && i == j && err == 0){
+                    printf("X");
+                    err = 1;}
             }
         }
         
@@ -50,6 +57,12 @@ int main(void)
         printf("\nMeus parabens, voce acertou\nA palavra era %s\n", palavraChave);}
     else{
         printf("\nInfelizmente suas chances acabaram, voce perdeu\nA palavra era %s\n",palavraChave);}
-
+    printf("\n\nQuer jogar novamente? \n[1] Sim\n[0] Não");
+    scanf("%d", &tent);
+    if (tent == 1){tentativas = 0;}
+    else{
+        printf("\nObrigado por jogar, volte sempre!!\n");
+    }
+}
     return 0;
 }
